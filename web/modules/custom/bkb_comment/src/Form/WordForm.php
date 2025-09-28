3bc76e5b1478210a32524ae5f155adb43053ee60
Milos Sramek <msramek22@gmail.com>
2025-09-28 17:27:26 +0000

    WordForm (subor komentarov): on save redirect to word view

--- web/modules/custom/bkb_comment/src/Form/WordForm.php
+++ web/modules/custom/bkb_comment/src/Form/WordForm.php
@@ -35,7 +35,8 @@ final class WordForm extends ContentEntityForm {
    */
   public function save(array $form, FormStateInterface $form_state): int {
     $result = parent::save($form, $form_state);
-    $form_state->setRedirect('view.comments_overview.page');
+    #$form_state->setRedirect('view.comments_overview.page');
+    $form_state->setRedirectUrl($this->entity->toUrl('canonical'));
 
     $message_args = ['%label' => $this->entity->toLink()->toString()];
     $logger_args = [
@@ -63,26 +64,28 @@ final class WordForm extends ContentEntityForm {
     }
 
     /** @var \Drupal\bkb_base\Helper $helper */
-    $helper = \Drupal::service('bkb_base.helper');
-    $excluded = [];
-    $values = $form_state->getValues();
+    /* if not commented out, it returns to a funny source editing form */
+    //$helper = \Drupal::service('bkb_base.helper');
+    //$excluded = [];
+    //$values = $form_state->getValues();
 
-    foreach ($values['comments'] as $i => $comment) {
-      if (is_numeric($i)) {
-        $excluded_sources = $helper->isSourceNew($comment['inline_entity_form']['sources']);
-        if ($excluded_sources !== FALSE) {
-          $excluded = array_unique(array_merge($excluded, $excluded_sources));
-        }
-      }
-    }
+    //foreach ($values['comments'] as $i => $comment) {
+      //if (is_numeric($i)) {
+        //$excluded_sources = $helper->isSourceNew($comment['inline_entity_form']['sources']);
+        //if ($excluded_sources !== FALSE) {
+          //$excluded = array_unique(array_merge($excluded, $excluded_sources));
+        //}
+      //}
+    //}
 
-    if (!empty($excluded)) {
-      $form_state->setRedirect(
-        'entity.source.data.edit',
-        ['id' => $this->entity->id()],
-        ['query' => ['excluded' => implode(',', $excluded)]]
-      );
-    }
+    //if (!empty($excluded)) {
+      //$form_state->setRedirect(
+        //'entity.source.data.edit',
+        //['id' => $this->entity->id()],
+        //['query' => ['excluded' => implode(',', $excluded)]]
+      //);
+    //}
+
 
     return $result;
   }
